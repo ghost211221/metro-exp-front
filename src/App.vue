b023aaf693692a7d05cb0c712ec22e3202d1200e
Ilya Solovyev <solovyev.21@yandex.ru>
2020-07-25 13:01:48 +0300

    dist

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
     return {
       showInitFileDialog: false,
       selectedFile: null,
-      API: ' ghost2112.pythonanywhere.com',
+      API: 'http://ghost2112.pythonanywhere.com',
       documentHTML: '',
       showDocumentText: false,
       documentID: 0,
